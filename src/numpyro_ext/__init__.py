28e9ed842f2caad5bef491a9fd9528ff11495b15
Dan Foreman-Mackey <dfm@dfm.io>
2023-11-14 15:36:13 -0500

    Lint

--- src/numpyro_ext/__init__.py
+++ src/numpyro_ext/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from numpyro_ext.version import __version__ as __version__
 from numpyro_ext import distributions as distributions
 from numpyro_ext import infer as infer
 from numpyro_ext import optim as optim
 from numpyro_ext.info import information as information
+from numpyro_ext.version import __version__ as __version__
